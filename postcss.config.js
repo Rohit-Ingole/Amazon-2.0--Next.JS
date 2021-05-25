944820b2d59bfc7a926ba2beb50fad6ea2e92d54
Rohit-Ingole <rvingole68@gmail.com>
2021-05-25 23:37:37 +0530

    homepage

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,16 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+const cssnano = require('cssnano')
+
 module.exports = {
-  plugins: ["tailwindcss", "autoprefixer"],
-};
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    cssnano({
+      preset: 'default'
+    }),
+    purgecss({
+      content: ['./public/*.html'],
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+    })
+  ]
+}
